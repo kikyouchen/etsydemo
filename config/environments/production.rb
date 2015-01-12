1ee9ee9cf0959a24fdcdde248a6e342fec39aac7
j chen <kikyouchen@gmail.com>
2015-01-12 12:49:56 -0500

    Install & setup devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # devise remember to change localhose:3000 to actual domain name
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
